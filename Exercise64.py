9406cdf5cf3c78e61ea9b54340cc5e2ce3c7bce9
DiemMyKHDL <lehoaitrinh67@gmail.com>
2023-11-18 22:25:40 +0700

    Exercise

--- Exercise64.py
+++ Exercise64.py
@@ -9,8 +9,8 @@ while True:
     remainder=pennies%5
     if remainder==0: the_pennis_payment_amout=pennies
     elif remainder <2.5:
-        sodongxuphaitra=pennies - remainder
+        the_pennis_payment_amout=pennies - remainder
     else:
-        sodongxuphaitra=pennies + (5-remainder)
+        the_pennis_payment_amout=pennies + (5-remainder)
 print('The total cost of all the entered items is: $', total_cost, sep='')
 print('The cash payment amount: $',the_pennis_payment_amout/100, sep='')
